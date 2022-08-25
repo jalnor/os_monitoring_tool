3f610fe644acdad723f3586714c2479767892f79
Bob Belderbos <bobbelderbos@gmail.com>
2022-08-25 15:49:04 +0200

    coding session

--- MyDb.py
+++ MyDb.py
@@ -4,33 +4,26 @@ import os
 import test
 
 
-def setup_table(self):
-    self.my_cursor.execute(
-        "CREATE TABLE IF NOT EXISTS processes (name VARCHAR(255), proc_id INT(32), status VARCHAR(255), start_time "
-        "TIME)")
-
-
-def add_process(self, name, proc_id, status, started):
-    self.my_cursor.execute(
-            "INSERT INTO processes(name, proc_id, status, started) "
-            "Values(%s,%s,%s,%s)", (name, proc_id, status, started))
-    self.mydb.commit()
-
-
 class MyDb:
-    my_cursor: cursor
-    mydb: mysql.connector
 
     def __init__(self):
         mydb = mysql.connector.connect(
             host="localhost",
-            user="jnor42",
+            user=os.environ["db_user"],
             password=os.environ["db_pword"],
-            database="os_monitoring_tool"
+            database=os.environ["db"]
         )
-
+        my_cursor = self.mydb.cursor()
         print('In there')
+        self.setup_table()
 
-        my_cursor = mydb.cursor()
-        setup_table(my_cursor)
+    def setup_table(self):
+        self.my_cursor.execute(
+            "CREATE TABLE IF NOT EXISTS processes (name VARCHAR(255), proc_id INT(32), status VARCHAR(255), start_time "
+            "TIME)")
 
+    def add_process(self, proc):
+        self.my_cursor.execute(
+                "INSERT INTO processes(name, proc_id, status, started) "
+                "Values(%s,%s,%s,%s)", (proc.name, proc.proc_id, proc.status, proc.started))
+        self.mydb.commit()
--- test.py
+++ test.py
@@ -6,6 +6,8 @@ import mysql
 from mysql.connector import cursor
 import psutil
 
+import MyDb
+
 
 @dataclass
 class Process:
@@ -15,29 +17,9 @@ class Process:
     started: str
 
 
-def mydb_connection() -> mysql.connector:
-    return mysql.connector.connect(
-        host="localhost",
-        user=os.environ["db_user"],
-        password=os.environ["db_pword"],
-        database=os.environ["db"]
-    )
-
-
-def add_process_to_db(func_proc: Process, func_mydb: mysql.connector, func_cursor: mysql.connector.cursor):
-    func_cursor.execute(
-        "INSERT INTO processes(name, proc_id, status, started) "
-        "Values(%s,%s,%s,%s)", (func_proc.name, func_proc.proc_id, func_proc.status, func_proc.started))
-    func_mydb.commit()
-
-
 if __name__ == "__main__":
-
     procs = {p for p in psutil.process_iter(['name', 'pid', 'status'])}
-
-    mydb = mydb_connection()
-    my_cursor = mydb.cursor()
-
+    my_db = MyDb()
     for p in procs:
         proc = Process(p.name(), p.pid, p.status(), dt.datetime.fromtimestamp(p.create_time()))
-        add_process_to_db(proc, mydb, my_cursor)
+        my_db.add_process(proc)
